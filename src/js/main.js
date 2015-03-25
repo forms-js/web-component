4e7f695a36a8fb49994fce10ca688eb2276a6499
Mohsen Azimi <msn.azi@gmail.com>
2015-03-25 14:32:20 -0700

    Use pure ES6 class

--- src/js/main.js
+++ src/js/main.js
@@ -1,48 +1,48 @@
-const proto = Object.create(HTMLFormElement.prototype);
-
 const importDoc = document.currentScript.ownerDocument;
 
-proto.createdCallback = function() {
-  const template = importDoc.querySelector('template');
-  const templateContent = document.importNode(template.content, true);
+class FormsJS extends HTMLFormElement {
+  createdCallback() {
+    const template = importDoc.querySelector('template');
+    const templateContent = document.importNode(template.content, true);
 
-  this.createShadowRoot().appendChild(templateContent);
+    this.createShadowRoot().appendChild(templateContent);
 
-  this.model      = this.getAttribute('model');
-  this.validation = this.getAttribute('validation');
-  this.view       = this.getAttribute('view');
+    this.model      = this.getAttribute('model');
+    this.validation = this.getAttribute('validation');
+    this.view       = this.getAttribute('view');
 
-  // TODO JSON.parse model, validation and view
+    // TODO JSON.parse model, validation and view
 
-  this.form = new formsjs.Form(this.model, this.validation, this.view);
-};
-
-proto.attachedCallback = function() {
-  // TODO;
-};
-
-proto.detachedCallback = function() {
-  // TODO
-};
-
-proto.attributeChangedCallback = function(attrName, oldValue, newValue) {
-  //  TODO
-};
-
-proto.getFormData = function(){
-  const data = new FormData();
-
-  if (this.form.formData && typeof this.form.formData === 'object') {
-    Object.keys(this.form.fromData).forEach( (key) => {
-      data.append(key, this.form.formData[key]);
-    });
+    this.form = new formsjs.Form(this.model, this.validation, this.view);
   }
 
-  return data;
+  attachedCallback() {
+    // TODO;
+  }
+
+  detachedCallback() {
+    // TODO
+  }
+
+  attributeChangedCallback(attrName, oldValue, newValue) {
+    //  TODO
+  };
+
+  getFormData(){
+    const data = new FormData();
+
+    if (this.form.formData && typeof this.form.formData === 'object') {
+      Object.keys(this.form.fromData).forEach( (key) => {
+        data.append(key, this.form.formData[key]);
+      });
+    }
+
+    return data;
+  }
 }
 
 // Register element and expose DOM API globally
-window.FormsJS = document.registerElement('forms-js', {
-  prototype: proto,
+window.FormsJSElement = document.registerElement('forms-js', {
+  prototype: FormsJS.prototype,
   'extends': 'form'
 });
